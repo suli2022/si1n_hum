d76ce175f819090c31e242528f589394a8af37a0
Nagy Janos <nagy@zold.lan>
2023-04-04 16:48:36 +0200

    Adatbázis kész

---
+++ database/create.sql
@@ -0,0 +1,18 @@
+create database hum
+character set utf8
+collate utf8_hungarian_ci;
+
+use hum;
+
+create table employees (
+    id int not null primary key auto_increment,
+    name varchar(50),
+    city varchar(50),
+    salary double
+);
+
+grant all privileges
+on hum.*
+to hum@localhost
+identified by 'titok'; 
+
---
+++ database/data.sql
@@ -0,0 +1,8 @@
+insert into employees 
+(name, city, salary)
+values
+('Erős István', 'Szeged', 384),
+('Piros Irén', 'Szeged', 384),
+('Tilos Péter', 'Szolnok', 394),
+('Alvó Katalin', 'Tata', 381),
+('Dankó István', 'Szeged', 364);
